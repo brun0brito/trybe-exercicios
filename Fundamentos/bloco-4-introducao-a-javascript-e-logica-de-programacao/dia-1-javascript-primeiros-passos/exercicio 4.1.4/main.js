ed000e520bde96cafd6bfaba7db43cb7b3381e41
Bruno <brunobrito728@gmail.com>
2022-07-20 15:52:31 -0300

    Exercício 4.1.4 finalizado

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio 4.1.4/main.js
@@ -0,0 +1,17 @@
+/*Faça um programa que, dado um valor definido numa 
+constante, retorne "positive" se esse valor for 
+positivo, "negative" se for negativo e "zero"
+ caso contrário. */
+
+ const n1 = 50;
+ let resultado;
+
+ if ( n1 > 0){
+  resultado = "Positive"
+ } else if ( n2 < 0){
+  resultado = "Negative"
+ } else {
+  resultado = "Zer0"
+ }
+
+ console.log(resultado)
